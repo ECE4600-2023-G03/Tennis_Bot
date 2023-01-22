fe5cfd2ff731c71a7848da7749051e5cd8742b9b
Ryan Shaski <ryanshaski@gmail.com>
2023-01-22 11:43:06 -0600

    state machine integration

--- src/tennis_pal_bringup/launch/full_bot.py
+++ src/tennis_pal_bringup/launch/full_bot.py
@@ -25,10 +25,21 @@ def generate_launch_description():
         package="uart-bridge",
         executable="uart_bridge"
     )
+    pilot = Node(
+        package="pilot",
+        executable="pilot"
+    )
+
+    state_machine = Node(
+        package="state_machine",
+        executable="state_machine"
+    )
 
     ld.add_action(path_plan_node)
     ld.add_action(control_node)
     ld.add_action(detection)
     ld.add_action(uart_bridge)
+    ld.add_action(pilot)
+    ld.add_action(state_machine)
 
     return ld
