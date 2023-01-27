ce1eaedd8476b145eb9cf0c45a887fa52d1e61a6
Ryan Shaski <ryan.shaski@gmail.com>
2023-01-27 10:08:55 -0600

    Fixed docker file for website

--- website/app.js
+++ website/app.js
@@ -1,38 +1,17 @@
-// const http = require('http')
-// const fs = require('fs')
-// const port = 3000
-
-// const server = http.createServer(function(req,res) {
-//     res.writeHead(200, {'Content-type': 'text/html'})
-//     fs.readFile('index.html', function(error, data){
-//         if (error) {
-//             res.writeHead(404)
-//             res.write('Error: File Not Found')
-//         }
-//         else {
-//             res.write(data)
-//         }
-//     })
-// })
-
-// server.listen(port, function(error) {
-//     if (error) {
-//         console.log('Something went wrong', error)
-//     }
-//     else {
-//         console.log('Server is listening on port ' + port)
-//     }
-// })
-
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
-// sendFile will go here
+// index
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+// devpage
+app.get('/devpage', function(req, res) {
+  res.sendFile(path.join(__dirname, '/devpage.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
--- website/public/js/functions.js
+++ website/public/js/functions.js
@@ -1,25 +1,207 @@
 // When we receive a message on /my_topic, add its data as a list item to the "messages" ul
-function publish() {
+function start_collection() {
     // Create a listener for /my_topic
-    const my_topic_listener = new ROSLIB.Topic({
+    const status_topic = new ROSLIB.Topic({
         ros,
-        name: "/my_topic",
+        name: "/status_topic",
         messageType: "std_msgs/String",
     });
 
     var str = new ROSLIB.Message({
-        data: 'hello'
+        data: 'Collecting'
     });
 
-    my_topic_listener.publish(str);
+    status_topic.publish(str);
 
-    my_topic_listener.subscribe((message) => {
-        const ul = document.getElementById("messages");
-        const newMessage = document.createElement("p");
-        newMessage.appendChild(document.createTextNode(str.data));
-        ul.appendChild(newMessage);
+    status_topic.subscribe((message) => {
+        document.getElementById("messages").innerHTML = message.data;
     });
+
+    // Create a listener for /twist_topic
+    const bool_topic = new ROSLIB.Topic({
+        ros,
+        name: "/bool_topic",
+        messageType: "std_msgs/msg/Bool"
+    });
+
+    var bol = new ROSLIB.Message({
+        data: true
+    });
+
+    bool_topic.publish(bol);
+
+    // When we receive a message on /twist_topic, add its data as a list item to the “twist_messages" ul
+    bool_topic.subscribe((message) => {
+        document.getElementById("bool_messages").innerHTML = message.data;
+    });
+}
+
+function go_home() {
+    // Create a listener for /my_topic
+    const status_topic = new ROSLIB.Topic({
+        ros,
+        name: "/status_topic",
+        messageType: "std_msgs/String",
+    });
+
+    var str = new ROSLIB.Message({
+        data: 'Going back Home'
+    });
+
+    status_topic.publish(str);
+
+    status_topic.subscribe((message) => {
+        document.getElementById("messages").innerHTML = message.data;
+    });
+
+    // Create a listener for /twist_topic
+    const bool_topic = new ROSLIB.Topic({
+        ros,
+        name: "/bool_topic",
+        messageType: "std_msgs/msg/Bool"
+    });
+
+    var bol = new ROSLIB.Message({
+        data: false
+    });
+
+    bool_topic.publish(bol);
+
+    // When we receive a message on /twist_topic, add its data as a list item to the “twist_messages" ul
+    bool_topic.subscribe((message) => {
+        document.getElementById("bool_messages").innerHTML = message.data;
+    });
+}
+
+function emer_stop() {
+    // Create a listener for /my_topic
+    const status_topic = new ROSLIB.Topic({
+        ros,
+        name: "/status_topic",
+        messageType: "std_msgs/String",
+    });
+
+    var str = new ROSLIB.Message({
+        data: 'Stopping'
+    });
+
+    status_topic.publish(str);
+
+    status_topic.subscribe((message) => {
+        document.getElementById("messages").innerHTML = message.data;
+    });
+
+    // Create a listener for /twist_topic
+    const bool_topic = new ROSLIB.Topic({
+        ros,
+        name: "/bool_topic",
+        messageType: "std_msgs/msg/Bool"
+    });
+
+    var bol = new ROSLIB.Message({
+        data: false
+    });
+
+    bool_topic.publish(bol);
+
+    // When we receive a message on /twist_topic, add its data as a list item to the “twist_messages" ul
+    bool_topic.subscribe((message) => {
+        document.getElementById("bool_messages").innerHTML = message.data;
+    });
+}
+
+function manual_up() {
+    alert('UP');
+    const twist_topic = new ROSLIB.Topic({
+        ros,
+        name: "/manual_speed",
+        messageType: "geometry_msgs/msg/Twist"
+    });
+
+    var twist = new ROSLIB.Message({
+        linear: {
+            x: 1.0,
+            y: 0.0,
+            z: 0.0
+        },
+        angular: {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0
+        }
+    });
+
+    twist_topic.publish(twist);
+}
+
+function manual_down() {
+    alert('DOWN');
+    const twist_topic = new ROSLIB.Topic({
+        ros,
+        name: "/manual_speed",
+        messageType: "geometry_msgs/msg/Twist"
+    });
+
+    var twist = new ROSLIB.Message({
+        linear: {
+            x: -1.0,
+            y: 0.0,
+            z: 0.0
+        },
+        angular: {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0
+        }
+    });
+    twist_topic.publish(twist);
 
 }
 
+function manual_left() {
+    alert('LEFT');
+    const twist_topic = new ROSLIB.Topic({
+        ros,
+        name: "/manual_speed",
+        messageType: "geometry_msgs/msg/Twist"
+    });
 
+    var twist = new ROSLIB.Message({
+        linear: {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0
+        },
+        angular: {
+            x: 0.0,
+            y: 0.0,
+            z: 1.0
+        }
+    });
+    twist_topic.publish(twist);
+
+}
+
+function manual_right() {
+    alert('RIGHT');
+    const twist_topic = new ROSLIB.Topic({
+        ros,
+        name: "/manual_speed",
+        messageType: "geometry_msgs/msg/Twist"
+    });
+
+    var twist = new ROSLIB.Message({
+        linear: {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0
+        },
+        angular: {
+            x: 0.0,
+            y: 0.0,
+            z: -1.0
+        }
+    });
+    twist_topic.publish(twist);
+
+}
